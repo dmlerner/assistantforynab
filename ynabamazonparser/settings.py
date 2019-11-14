813a24c8fcd9ea49a7a437040f12e280b89a79e3
David Lerner <dmlerner@gmail.com>
2019-11-13 20:04:25 -0800

    fixed bug with private settings when script run from outside its folder

--- ynabamazonparser/settings.py
+++ ynabamazonparser/settings.py
@@ -17,7 +17,7 @@ chrome_data_dir = os.path.join(home, '\\AppData\\Local\\Google\\Chrome\\User Dat
 ' NOTE: must have the chromedriver named chromedriver (not chromedriver.exe), somewhere in your PATH '
 
 downloads_path = os.path.join(home, 'Downloads')
-script_path = os.getcwd()
+script_path  = os.path.split(os.path.realpath(__file__))[0]
 log_path = os.path.join(script_path, 'log')
 data_path = os.path.join(script_path, 'data')
 for p in log_path, data_path:
@@ -25,10 +25,10 @@ for p in log_path, data_path:
         os.mkdir(p)
 
 private_settings_path = os.path.join(script_path, 'private_settings.py')
-private_settings_template_path = os.path.join(script_path, 'private_settings_template.py')
 if not os.path.exists(private_settings_path):
     utils.log('ERROR: no private settings found, using defaults. Please edit %s' % private_settings_path)
     utils.log('Will not be able to access YNAB')
+    private_settings_template_path = os.path.join(script_path, 'private_settings_template.py')
     shutil.copy(private_settings_template_path, private_settings_path)
 
 budget_id = private_settings.budget_id
@@ -41,7 +41,7 @@ default_category = 'ERROR'
 close_browser_on_finish = False
 
 ' Always redownload csvs '
-force_download_amazon = False
+force_download_amazon = True
 
 ' TODO: variable controlling if transactions stay checked in the UI when done; currently they do '
 ' TODO: actually use log levels '
