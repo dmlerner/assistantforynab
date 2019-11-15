f1f9b255440ca2714da0217a54b8f9ab7e5ef851
David Lerner <dmlerner@gmail.com>
2019-11-15 00:45:32 -0800

    and yet again I published my api key and got to generate a new one

--- ynabamazonparser/config/private.py
+++ ynabamazonparser/config/private.py
@@ -3,4 +3,4 @@ test_budget  = 'cd08c66a-7cfd-4f74-b5fd-ef03f3ac7a8a'
 minimal_budget = '0b9fecaf-a02f-4ba2-9743-804ed4f7f1d2'
 budget_id = test_budget
 account_name = 'Chase Amazon'
-api_key = '0e3489474f21a27a8c7f5ab0cedfd8eb9b807f6467a2f36da0a656fdf1e010c9'
+api_key = 'e0b2c7fe5b4759c9a514e0d5e341ba084a4b47286c9dc33946a2bcde34e0a91e'
