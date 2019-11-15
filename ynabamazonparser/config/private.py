dc35844edeb6cc4aaf4bd960fb1e9b3e987a9a30
David Lerner <dmlerner@gmail.com>
2019-11-15 00:53:57 -0800

    .

--- ynabamazonparser/config/private.py
+++
@@ -1,7 +0,0 @@
-real_budget = '7b027e9b-4ed8-495e-97bd-f0339357adf0'
-test_budget  = 'cd08c66a-7cfd-4f74-b5fd-ef03f3ac7a8a'
-minimal_budget = '0b9fecaf-a02f-4ba2-9743-804ed4f7f1d2'
-budget_id = test_budget
-account_name = 'Chase Amazon'
-api_key = 'a1809e230105e3e6ff4b27d35c8b699897a2043fc0f883791d4e6d0cb71bf293'
-
