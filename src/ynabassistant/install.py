44dd3b13215b80c3a80e1f3cbfe18c9fb24a3e1f
David Lerner <dmlerner@gmail.com>
2019-12-22 12:40:59 -0800

    log, not print in install

--- src/ynabassistant/install.py
+++ src/ynabassistant/install.py
@@ -7,29 +7,26 @@ import requests
 import re
 
 
-from ynabassistant.utils import gui
+from ynabassistant.utils import gui, utils
 
 
 def clean():
-    print('clean')
+    utils.log_debug('clean')
     for p in [settings.chromedriver_path, settings.settings_path]:
         if os.path.exists(p):
-            print('removing', p)
+            utils.log_debug('removing', p)
             os.remove(p)
 
     for p in settings.log_dir, settings.data_dir, settings.backup_dir:
         if os.path.exists(p):
-            print('removing', p)
+            utils.log_debug('removing', p)
             shutil.rmtree(p)
 
     settings.init(use_defaults=True)
 
 
-# TODO: log, not print
-
-
 def setup_chromedriver():
-    print('setup_chromedriver')
+    utils.log_debug('setup_chromedriver')
     if os.path.exists(settings.chromedriver_path):
         return
     chromedriver_url = 'https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_win32.zip'
@@ -44,46 +41,47 @@ def setup_chromedriver():
 
 
 def setup_ynab_auth():
-    print('setup_ynab_auth')
+    utils.log_debug('setup_ynab_auth')
     # TODO: make this use oauth instead of api tokens
     if settings.get('api_token'):
         return
     api_token_url = 'https://app.youneedabudget.com/settings/developer'
     d = gui.driver()
     d.get(api_token_url)
+    utils.log_info('Log in')
     new_token_button = gui.get_by_text('button', 'New Token')
     gui.click(new_token_button)
-    password_box = gui.driver().find_element_by_id('user_current_password')
-    gui.click(password_box)
-    print('Enter your password and click "Generate"')
+    utils.log_info('Enter your password in the YNAB Web App, then click "Generate"')
     while 'New Personal Access Token' not in d.page_source:
         time.sleep(.5)
     api_token = re.search('New Personal Access Token: <strong>([^<]*)</strong>', d.page_source).groups()[0]
     settings.set('api_token', api_token)
-    print('api_token=', settings.api_token)
+    utils.log_debug('api_token=', settings.api_token)
     assert settings.api_token
 
 
 def setup_ynab_budget_id():
-    print('setup_ynab_budget_id')
+    utils.log_debug('setup_ynab_budget_id')
     if settings.get('budget_id'):
         return
     url = 'https://app.youneedabudget.com/'
     driver = gui.driver()
     driver.get(url)
-    print('Log in')
+    utils.log_info('Log in')
     while not re.search('youneedabudget.com/([^/]+)/', driver.current_url):
         time.sleep(.5)
-    input('Press Enter when you have loaded the budget you want to use.')
+    budget_selection_prompt = 'Press Enter when you have loaded the budget you want to use.'
+    input(budget_selection_prompt)
+    utils.log_debug(budget_selection_prompt)
     while not re.search('youneedabudget.com/([^/]+)/', driver.current_url):
         time.sleep(.5)
     settings.set('budget_id', re.search('youneedabudget.com/([^/]+)/', driver.current_url).groups()[0])
-    print('settings.budget_id=', settings.budget_id)
+    utils.log_debug('settings.budget_id=', settings.budget_id)
     assert settings.budget_id
 
 
 def make_dirs():
-    print('make_dirs')
+    utils.log_debug('make_dirs')
     for p in settings.log_dir, settings.data_dir, settings.backup_dir:
         if not os.path.exists(p):
             os.mkdir(p)
@@ -98,6 +96,6 @@ def do(should_clean=False):
     setup_ynab_auth()
     setup_ynab_budget_id()
     settings.init()
-    print(vars(settings).keys())
-    print('at end of install, api_token=', settings.get('api_token'), settings.api_token)
+    utils.log_debug(vars(settings).keys())
+    utils.log_debug('at end of install, api_token=', settings.get('api_token'), settings.api_token)
     gui.driver().close()
